3747484389d6c7908f3de110d2d8e0f8645608dc
Kenny <ktran604@hotmail.com>
2017-11-03 11:58:49 -0700

    added ball movement, paddle collision

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -7,17 +7,74 @@ export default class Ball {
     this.boardWidth = boardWidth;
     this.boardHeight = boardHeight;
     this.direction = 1;
-   
+    this.reset();
   }
 
+  // resets ball at the center, randomly generates a number to throw the ball to the right
+  reset() {
+    this.x = this.boardWidth / 2;
+    this.y = this.boardHeight / 2;
 
-  render(svg) {
+    this.vy = 0;
+    while (this.vy === 0) {
+      this.vy = Math.floor(Math.random() * 10 - 5);
+    }
+
+    // this.vx = 6 - 5;
+    this.vx = this.direction * (6 - Math.abs(this.vy));
+  }
+
+  wallCollsion() {
+    const hitLeft = this.x - this.radius <= 0;
+    const hitRight = this.x + this.radius >= this.boardWidth;
+    const hitTop = this.y + this.radius <= 0;
+    const hitBottom = this.y - this.radius >= this.boardHeight;
+
+    if (hitLeft || hitRight) {
+      this.vx = -this.vx
+
+    } else if (hitTop || hitBottom) {
+      this.vy = -this.vy
+
+    }
+  }
+
+  paddleCollision(paddle1, paddle2) {
+    if (this.vx > 0) {
+      //right collision p2.x + b.r
+      let paddle = paddle2.coordinates(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+      let {leftX, topY, bottomY} = paddle;
+      if (
+        this.x + this.radius >= leftX
+        && this.y >= topY
+        && this.y <= bottomY
+      ) {
+        this.vx = -this.vx
+      }
+    } else {
+      // left collision p1
+      let paddle = paddle1.coordinates(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+      let {rightX, topY, bottomY} = paddle;
+      if (
+        this.x - this.radius <= rightX
+        && this.y >= topY
+        && this.y <= bottomY
+      ) {
+        this.vx = -this.vx
+      }
+    }
+  }
+
+  render(svg, paddle1, paddle2) {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.wallCollsion();
+    this.paddleCollision(paddle1, paddle2);
     let ball = document.createElementNS(SVG_NS, 'circle');
-    ball.setAttributeNS(null, 'r',this.radius);
-    ball.setAttributeNS(null, 'cx', this.boardWidth/2);
-    ball.setAttributeNS(null, 'cy', this.boardHeight/2);
+    ball.setAttributeNS(null, 'r', this.radius);
+    ball.setAttributeNS(null, 'cx', this.x);
+    ball.setAttributeNS(null, 'cy', this.y);
     ball.setAttributeNS(null, 'fill', 'white');
-
     svg.appendChild(ball);
   }
 }
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,4 +1,4 @@
-import {SVG_NS,KEYS} from '../settings.js';
+import { SVG_NS, KEYS } from '../settings.js';
 
 
 import Board from './Board';
@@ -11,65 +11,77 @@ export default class Game {
 		this.element = element;
 		this.width = width;
 		this.height = height;
-	
-		
+
+
 		this.gameElement = document.getElementById(element);
 
 
 
-		this.board = new Board (this.width,this.height); 
-		
+		this.board = new Board(this.width, this.height);
+
 		this.boardGap = 10;
 		this.paddleWidth = 8;
 		this.paddleHeight = 56;
 
-		this.paddle1 = new Paddle (
+
+
+		this.paddle1 = new Paddle(
 			this.height,
 			this.paddleWidth,
 			this.paddleHeight,
 			this.boardGap,
-			(this.height-this.paddleHeight)/2,
+			(this.height - this.paddleHeight) / 2,
 			KEYS.a,
 			KEYS.z
 		);
 
-		this.paddle2 = new Paddle (
+		this.paddle2 = new Paddle(
 			this.height,
 			this.paddleWidth,
 			this.paddleHeight,
-			(this.width-this.boardGap-this.paddleWidth),
-			(this.height-this.paddleHeight)/2,
+			(this.width - this.boardGap - this.paddleWidth),
+			(this.height - this.paddleHeight) / 2,
 			KEYS.up,
 			KEYS.down
 		),
 
-		this.ball = new Ball (
-			8, 
+			this.radius = 8;
+
+		this.ball = new Ball(
+			this.radius,
 			this.width,
 			this.height
 		);
-	
 
+		document.addEventListener('keydown', event => {
+			if ( event.key === KEYS.spaceBar ) {
+				this.pause = !this.pause;
+			}
+		});
 	}
 
 
 
 	render() {
+
+		if (this.pause){
+			return;
+		}
+
 		this.gameElement.innerHTML = '';
 
 
-		let svg = document.createElementNS(SVG_NS,'svg');
-		svg.setAttributeNS(null,'width',this.width);
-		svg.setAttributeNS(null,'height',this.height);
-		svg.setAttributeNS(null,'viewBox',`0 0 ${this.width} ${this.height}`);
+		let svg = document.createElementNS(SVG_NS, 'svg');
+		svg.setAttributeNS(null, 'width', this.width);
+		svg.setAttributeNS(null, 'height', this.height);
+		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
 		svg.setAttributeNS(null, 'version', '1.1');
 
 		this.gameElement.appendChild(svg);
 		this.board.render(svg);
 		this.paddle1.render(svg);
 		this.paddle2.render(svg);
-		this.ball.render(svg);
-	
+		this.ball.render(svg, this.paddle1,this.paddle2);
 	}
 
 }
